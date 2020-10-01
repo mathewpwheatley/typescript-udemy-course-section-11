318bad70083d6e6e1e80c7a31a79f747a5779695
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-10-01 12:09:55 -0700

    Refactored defauts via static class methods

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import { MatchReader } from './MatchReader'
-import { CsvFileReader } from './CsvFileReader'
-import { ConsoleReport } from './reportTargets/ConsoleReport'
-import { HtmlReport } from './reportTargets/HtmlReport'
-import { WinsAnalysis } from './analyzers/WinsAnalysis'
 import { Summary } from './Summary'
 
-const csvFileReader = new CsvFileReader('football.csv')
-const matchReader = new MatchReader(csvFileReader)
+const matchReader = MatchReader.fromCsv('football.csv')
+const summary = Summary.winsAnalysisWithHtmlReport('Man United')
+
 matchReader.load()
-
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport())
-
 summary.buildAndPrintReport(matchReader.matches)
